cc93dc4d3ba308c4d51b1695c48c1a48701ed605
Steven Rosa <s.rosa21@icloud.com>
2022-01-12 17:22:54 -0500

    logger middleware added

--- api/server.js
+++ api/server.js
@@ -1,11 +1,16 @@
 const express = require('express');
 
 const server = express();
-
+server.use(express.json())
 // remember express by default cannot parse JSON in request bodies
 
 // global middlewares and the user's router need to be connected here
+const logger = (req, res, next) => {
+  console.log(`${req.method} to ${req.url} at [${new Date().toISOString}]`)
+  next()
+}
 
+server.use(logger)
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
 });
